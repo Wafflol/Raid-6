3edd7ca88b9973bb7163aa6780d63365ae933342
Connor Won <cywon2@gmail.com>
2024-10-05 17:54:34 -0700

    draft document centre

--- network-api-challenge/src/pages/Switch.jsx
+++ network-api-challenge/src/pages/Switch.jsx
@@ -1,16 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import { Tab, Tabs } from '@mui/material';
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
 import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
 import styles from './Switch.module.css';
+import { DocumentCentre } from "./DocumentCentre";
 
 export const Switch = () => {
   return (
     <Stack>
-      {/* <Routes> */}
-        {/* <Route path='' Component={}/> */}
-      {/* </Routes> */}
+      <div className={styles.topText}>
+        <Typography variant='h3'>Document Centre</Typography>
+      </div>
+      <Routes>
+        <Route path='/' Component={DocumentCentre}/>
+      </Routes>
       <Tabs className={styles.navTab}>
         <Tab className={[styles.singleTab, styles.leftTab]} icon={<DocumentScannerIcon className={styles.tabIcon}/>}/>
         <Tab className={styles.singleTab} icon={<SendIcon className={styles.tabIcon}/>}></Tab>
